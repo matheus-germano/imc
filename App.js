01100934ce8d37977465b5bb58114d96c1f2d1ff
Matheus Germano da Costa <matheus.ggcosta2014@gmail.com>
2021-01-29 13:49:13 -0300

    bottom menu started

--- App.js
+++ App.js
@@ -1,114 +1,54 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import { Text, StatusBar } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Home from './src/pages/Home';
+import Results from './src/pages/Results';
+import About from './src/pages/About';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const Tab = createBottomTabNavigator();
 
 const App: () => React$Node = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
+      <NavigationContainer>
+        <Tab.Navigator initialRouteName='home' tabBarOptions={{activeTintColor: '#114B5F'}}>
+          <Tab.Screen 
+            name='home' 
+            component={Home}
+            options={{
+              tabBarLabel: '',
+              tabBarIcon: ({ color, size }) => (
+                <Icon name="home" color={'#114B5F'} size={32} />
+              ),
+            }}
+          />
+          <Tab.Screen 
+            name='results' 
+            component={Results}
+            options={{
+              tabBarLabel: '',
+              tabBarIcon: ({ color, size }) => (
+                <Icon name="assignment" color={'#114B5F'} size={32} />
+              ),
+            }}
+          />
+          <Tab.Screen 
+            name='about' 
+            component={About}
+            options={{
+              tabBarLabel: '',
+              tabBarIcon: ({ color, size }) => (
+                <Icon name="help-outline" color={'#114B5F'} size={32} />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </NavigationContainer>
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
---
+++ src/pages/Results/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+export default function Results() {
+  return (
+    <Text>Results</Text>
+  )
+}
